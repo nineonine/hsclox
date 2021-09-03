41176fc6ddaee7d4dbfb4545577892ee056d1a7f
nineonine <mail4chemik@gmail.com>
2021-09-03 16:21:06 -0700

    Add OP_CONSTANT opcode

---
+++ src/c/value.h
@@ -0,0 +1,19 @@
+#ifndef hsclox_value_h
+#define hsclox_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+
+#endif
