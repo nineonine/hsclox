041270b0720c04be2f6645b1534301f0277a8214
nineonine <mail4chemik@gmail.com>
2021-09-10 11:41:57 -0700

    Fix division

--- src/c/compiler.c
+++ src/c/compiler.c
@@ -199,7 +199,7 @@ static void binary(bool canAssign) {
         case TOKEN_PLUS: emitByte(OP_ADD); break;
         case TOKEN_MINUS: emitByte(OP_SUBTRACT); break;
         case TOKEN_STAR: emitByte(OP_MULTIPLY); break;
-        case TOKEN_SLASH: emitByte(OP_SUBTRACT); break;
+        case TOKEN_SLASH: emitByte(OP_DIVIDE); break;
         default: return; // unreachable
     }
 }
